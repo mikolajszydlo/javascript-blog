858b35811604716ee755b77b4f055760b2e15802
Mikolaj Szydlo <mikolaj.szydlo@gmail.com>
2021-09-08 22:37:49 +0200

    Add function to add active class to active link

--- js/script.js
+++ js/script.js
@@ -5,21 +5,27 @@
     // })
 
     const titleClickHandle = function(event) {
+        const clickedElement = this;
         console.log('Link was clicked!');
-        console.log(event);
 
-        /* remove class 'active' from all article links  */
+        /* [DONE] remove class 'active' from all article links  */
+        
         const activeLinks = document.querySelectorAll('.titles a.active');
         for(let activeLink of activeLinks){
             activeLink.classList.remove('active');
         }
-        /* add class 'active' to the clicked link */
+        
+        /* [IN PROGRESS] add class 'active' to the clicked link */
+        this.classList.add('active');
+        console.log('clickedElement', clickedElement);
 
-        /* remove class 'active' from all articles */
+        /* [DONE] remove class 'active' from all articles */
+        
         const activeArticles = document.querySelectorAll('.posts .active');
         for(let activeArticle of activeArticles){
             activeArticle.classList.remove('active');
         }
+        
         /* get 'href' attribute from the clicked link */
 
         /* find the correct article using the selector (value of 'href' attribute) */
